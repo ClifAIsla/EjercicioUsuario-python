895f2c7270dd145e9668a4b2e6fda89a78b29285
ClifAIsla <clifa.isla@gmail.com>
2022-01-19 11:34:46 -0500

    Cuenta bancaria solo con una clase metodo

--- Usuario.py
+++ Usuario.py
@@ -1,3 +1,4 @@
+"""
 class Usuario:
     nombre_banco = "Banco del Pueblo"
     def __init__(self, name = "N/A", email = "N/A", balance_cuenta = 0):
@@ -7,9 +8,11 @@ class Usuario:
     
     def hacer_deposito( self, cantidad ):
         self.balance_cuenta = self.balance_cuenta + cantidad
+        return self
     
     def retiro (self, cantidad):
         self.balance_cuenta -= cantidad
+        return self
 
     def mostrar_balance( self ):
         print( f"Usuario: {self.name}, balance: S/.{self.balance_cuenta}")
@@ -17,3 +20,44 @@ class Usuario:
     def transfer_dinero(self, otro_usuario, cantidad):
         otro_usuario.hacer_deposito( cantidad )
         self.balance_cuenta -= cantidad
+"""
+
+class CuentaBancaria:
+    todas_las_cuentas = []
+
+    def __init__(self, balance = 0, tasa_interes = 0.01):
+        self.balance = balance
+        self.tasa_interes = tasa_interes
+        CuentaBancaria.todas_las_cuentas.append(self)
+
+    def deposito (self, cantidad):
+        self.balance += cantidad
+        return self
+    
+    def retiro (self, cantidad):
+        if self.balance > 0:
+            self.balance -= cantidad
+        else:
+            self.balance -= cantidad - 5
+            print("Fondos insuficientes: cobrando una tarifa de $5")
+        return self
+
+    def mostrar_info_cuenta (self):
+        print(f"Su Balance es ${self.balance}")
+        return self
+
+    def ganar_interes (self):
+        if self.balance > 0:
+            self.balance += (self.balance*self.tasa_interes)
+            #print(  f"El interes ganado es ${self.balance}")
+        return self
+
+    #def imprime_todo (self):
+        self.deposito(19).retiro(5).ganar_interes().mostrar_info_cuenta()
+
+    @classmethod
+    def dame_los_balances(cls):
+        for x in cls.todas_las_cuentas:
+            x.mostrar_info_cuenta()
+
+    
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+"""
 from Usuario import Usuario
 
 ricardo = Usuario( "Ricardo" , "ricardo.humoz@gmail.com", 100 )
@@ -36,6 +37,31 @@ print("-----------------------")
 jorge.mostrar_balance()
 ricardo.mostrar_balance()
 
+piero = Usuario("Piero","piro@gmail.com")
+pepe = Usuario("Pedro","pero@gmail.com")
 
+piero.hacer_deposito(100).hacer_deposito(1).retiro(51).mostrar_balance()
+"""
+from Usuario import CuentaBancaria
+"""
+usuario1 = CuentaBancaria(200)
+usuario1.deposito(150)
+usuario1.retiro(50)
+#usuario1.mostrar_info_cuenta()
+usuario1.ganar_interes()
 
+CuentaBancaria.dame_los_balances()
+"""
+usuario1 = CuentaBancaria(20,0.01)
+#usuario1.deposito(100).deposito(50).deposito(150).retiro(40).ganar_interes().mostrar_info_cuenta()
 
+print("---------------------------------------")
+
+usuario2 = CuentaBancaria(120,0.5)
+#usuario2.deposito(25).deposito(1000).retiro(280).retiro(75).ganar_interes().mostrar_info_cuenta()
+
+print("---------------------------------------")
+
+usuario3 = CuentaBancaria(100,0.01)
+
+CuentaBancaria.dame_los_balances()
