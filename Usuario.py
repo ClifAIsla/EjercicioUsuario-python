49f4f7b3bcd84d32ac6a256077d90c9f631c2234
ClifAIsla <clifa.isla@gmail.com>
2022-01-19 18:05:20 -0500

    Usuarios con las cuantas bancarias y el bonus sensei

--- Usuario.py
+++ Usuario.py
@@ -1,26 +1,167 @@
-"""
 class Usuario:
-    nombre_banco = "Banco del Pueblo"
-    def __init__(self, name = "N/A", email = "N/A", balance_cuenta = 0):
+    #OJO, LOS VALORES QUE AGREGAMOS EN BALANCE E INTERES SON POR DEFAULT
+    def __init__(self, name = "N/A", email = "N/A", balance = 0, interes = 0.01, tipo = "Ahorros"):
         self.name = name
         self.email = email
-        self.balance_cuenta = 0
+        self.tipo = { 
+            "CuentaAhorros" : CuentaBancaria(balance, interes),
+            "CuentaFondos" : CuentaBancaria(balance, interes)
+    }
     
-    def hacer_deposito( self, cantidad ):
-        self.balance_cuenta = self.balance_cuenta + cantidad
+    #ACTUALIZAMOS DEPOSITOS CON EL OBJETO DE LA CUENTA
+    def hacer_deposito( self, cantidad, tipo_cuenta ):
+        self.tipo[ tipo_cuenta ].deposito (cantidad)
+        print(f"{self.name} hizo un deposito de ${cantidad} hacia su {tipo_cuenta}")
+        return self
+    
+    #ACTUALIZAMOS RETIROS CON EL OBJETO DE LA CUENTA
+    def hacer_retiro (self, cantidad , tipo_cuenta):
+        self.tipo[tipo_cuenta].retiro(cantidad)
+        print(f"{self.name} hizo un retiro de ${cantidad} desde su {tipo_cuenta}")
+        return self
+
+    #ACTUALIZAMOS MOSTRAR BALANCE CON EL OBJETO DE LA CUENTA
+    def mostrar_balance( self, tipo_cuenta):
+        #print( f"Usuario: {self.name}, balance: $ {self.cuenta.balance}")
+        print( f"Usuario: {self.name}, balance: $ {self.tipo[tipo_cuenta].balance} en su {tipo_cuenta}")
+
+    
+    def transfer_dinero(self, otro_usuario, cantidad, tipo_cuenta ):
+        otro_usuario.cuenta.deposito ( cantidad )
+        print(f"{self.name} hizo un transferencia de ${cantidad} a {otro_usuario.name}")
+        self.cuenta.retiro ( cantidad )
+        return self
+
+    #METODO CREAR CUENTA
+    def crear_cuenta ( self , tipo_cuenta, cantidad):
+        self.tipo[tipo_cuenta].balance = cantidad
+        print( f"El usuario {self.name} se esta creando una {self.tipo[tipo_cuenta]} con un balance de ${cantidad}" )
+        return self
+
+
+class CuentaBancaria:
+    todas_las_cuentas = []
+
+    def __init__(self, balance = 0, tasa_interes = 0.01):
+        self.balance = balance
+        self.tasa_interes = tasa_interes
+        CuentaBancaria.todas_las_cuentas.append(self)
+
+    def deposito (self, cantidad):
+        self.balance += cantidad
         return self
     
     def retiro (self, cantidad):
-        self.balance_cuenta -= cantidad
+        if self.balance > 0:
+            self.balance -= cantidad
+        else:
+            self.balance -= cantidad - 5
+            print("Fondos insuficientes: cobrando una tarifa de $5")
         return self
 
+    def mostrar_info_cuenta (self):
+        print(f"Su Balance es ${self.balance}")
+        return self
+
+    def ganar_interes (self):
+        if self.balance > 0:
+            self.balance += (self.balance*self.tasa_interes)
+        return self
+
+    @classmethod
+    def dame_los_balances(cls):
+        for x in cls.todas_las_cuentas:
+            x.mostrar_info_cuenta()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+class Usuario:
+    #OJO, LOS VALORES QUE AGREGAMOS EN BALANCE E INTERES SON POR DEFAULT
+    def __init__(self, name = "N/A", email = "N/A", balance = 0, interes = 0.01):
+        self.name = name
+        self.email = email
+        self.cuenta = CuentaBancaria (balance, interes)
+    
+    #ACTUALIZAMOS DEPOSITOS CON EL OBJETO DE LA CUENTA
+    def hacer_deposito( self, cantidad ):
+        #self.cuenta.balance += cantidad
+        self.cuenta.deposito ( cantidad )
+        #self.cuenta.mostrar_info_cuenta()
+        print(f"{self.name} hizo un deposito de ${cantidad}")
+        return self
+    
+    #ACTUALIZAMOS RETIROS CON EL OBJETO DE LA CUENTA
+    def retiro (self, cantidad):
+        #self.cuenta.balance -= cantidad
+        self.cuenta.retiro(cantidad)
+        print(f"{self.name} hizo un retiro de ${cantidad}")
+        return self
+
+    #ACTUALIZAMOS MOSTRAR BALANCE CON EL OBJETO DE LA CUENTA
     def mostrar_balance( self ):
-        print( f"Usuario: {self.name}, balance: S/.{self.balance_cuenta}")
+        print( f"Usuario: {self.name}, balance: $ {self.cuenta.balance}")
     
     def transfer_dinero(self, otro_usuario, cantidad):
-        otro_usuario.hacer_deposito( cantidad )
-        self.balance_cuenta -= cantidad
-"""
+        #otro_usuario.hacer_deposito( cantidad )
+        #otro_usuario.cuenta.balance += cantidad
+        otro_usuario.cuenta.deposito ( cantidad )
+        print(f"{self.name} hizo un transferencia de ${cantidad} a {otro_usuario.name}")
+        #self.cuenta.balance -= cantidad
+        self.cuenta.retiro ( cantidad )
+        return self
 
 class CuentaBancaria:
     todas_las_cuentas = []
@@ -59,5 +200,4 @@ class CuentaBancaria:
     def dame_los_balances(cls):
         for x in cls.todas_las_cuentas:
             x.mostrar_info_cuenta()
-
-    
+"""
--- main.py
+++ main.py
@@ -1,67 +1,63 @@
-"""
 from Usuario import Usuario
-
-ricardo = Usuario( "Ricardo" , "ricardo.humoz@gmail.com", 100 )
-jorge = Usuario( "Jorge" , "jorgetuterror@gmail.com", 1 )
-
-#Guido.nombre_banco = "Banco Interlank"
-#print(Guido.nombre_banco)
-
-# Accediendo a los atributos de la instancia
-#print(ricardo.name)	
-#print(jorge.email)
-
-#HACEMOS DEPOSITO
-jorge.hacer_deposito(100)
-jorge.hacer_deposito(1)
-print(jorge.balance_cuenta)
-
-#HACEMOS RETIRO
-jorge.retiro(51)
-print(jorge.balance_cuenta)
-
-#MOSTRAR BALANCE
-jorge.mostrar_balance()
-ricardo.mostrar_balance()
-#HACEMOS PRUEBA DE LA TRANSFERENCIA
-
-jorge.transfer_dinero( ricardo, 50)
-jorge.mostrar_balance()
-ricardo.mostrar_balance()
-
-#HACEMOS OTRA PUREBA
-ricardo.hacer_deposito(1000)
-ricardo.transfer_dinero( jorge , 2000)
-
-print("-----------------------")
-jorge.mostrar_balance()
-ricardo.mostrar_balance()
-
-piero = Usuario("Piero","piro@gmail.com")
-pepe = Usuario("Pedro","pero@gmail.com")
-
-piero.hacer_deposito(100).hacer_deposito(1).retiro(51).mostrar_balance()
-"""
 from Usuario import CuentaBancaria
+
 """
-usuario1 = CuentaBancaria(200)
-usuario1.deposito(150)
-usuario1.retiro(50)
-#usuario1.mostrar_info_cuenta()
-usuario1.ganar_interes()
+diccionario = {
+    "CuentaAhorros" : (100, 0.5),
+    "CuentaFondos" : (200, 0.10)
+}
 
-CuentaBancaria.dame_los_balances()
+print(diccionario["CuentaAhorros"][0])
 """
-usuario1 = CuentaBancaria(20,0.01)
-#usuario1.deposito(100).deposito(50).deposito(150).retiro(40).ganar_interes().mostrar_info_cuenta()
 
-print("---------------------------------------")
+#CASO RICARDO
+ricardo = Usuario( "Ricardo")
 
-usuario2 = CuentaBancaria(120,0.5)
-#usuario2.deposito(25).deposito(1000).retiro(280).retiro(75).ganar_interes().mostrar_info_cuenta()
+print("---------------------")
 
-print("---------------------------------------")
+ricardo.crear_cuenta ( "CuentaAhorros", 30 ).hacer_retiro (25, "CuentaAhorros").hacer_deposito(100,"CuentaAhorros")
 
-usuario3 = CuentaBancaria(100,0.01)
+print("---------------------")
 
-CuentaBancaria.dame_los_balances()
+ricardo.crear_cuenta ( "CuentaFondos",100).hacer_deposito(100,"CuentaFondos").hacer_deposito(25,"CuentaFondos").hacer_retiro(5,"CuentaFondos")
+
+print("---------------------")
+
+ricardo.mostrar_balance("CuentaFondos")
+ricardo.mostrar_balance("CuentaAhorros")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+ricardo = Usuario( "Ricardo" , "ricardo@gmail.com", 100, 0.05 )
+#CuentaRi = CuentaBancaria( 100, 0.05 )
+ricardo.hacer_deposito( 150 ).retiro( 50 ).mostrar_balance()
+print("--------------------")
+ander = Usuario("Anderson", "anger@gmail.com", 200, 0.01)
+
+ricardo.transfer_dinero(ander, 100).mostrar_balance()
+print("--------------------")
+"""
